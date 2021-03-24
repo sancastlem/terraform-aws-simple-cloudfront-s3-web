f3973cddb8118d87d3116b6c2aa15b8a59483d42
Sandra Castillo <sancastlemen@gmail.com>
2021-03-24 08:43:09 +0100

    feat: adapted code to support TF 0.12

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@
 resource "aws_s3_bucket" "web-s3-bucket" {
   bucket = "web-${var.name}-bucket"
   acl    = "public-read"
-  region = "${var.region}"
+  region = var.region
 
-  tags {
+  tags = {
     Name        = "web-${var.name}"
-    Environment = "${var.environment}"
+    Environment = var.environment
   }
 
   versioning {
@@ -23,9 +23,9 @@ resource "aws_s3_bucket" "web-s3-bucket" {
 ## Cloudfront
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   origin {
-    domain_name = "${aws_s3_bucket.web-s3-bucket.bucket_regional_domain_name}"
+    domain_name = aws_s3_bucket.web-s3-bucket.bucket_regional_domain_name
 
-    origin_path = "${var.cloudfront_origin_path}"
+    origin_path = var.cloudfront_origin_path
     origin_id   = "s3-${var.name}"
   }
 
@@ -34,7 +34,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   comment             = ""
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
-  aliases             = ["${var.url}"]
+  aliases             = [var.url]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -58,12 +58,12 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     }
   }
 
-  tags {
-    Environment = "${var.environment}"
+  tags = {
+    Environment = var.environment
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${var.ssl_arn}"
+    acm_certificate_arn = var.ssl_arn
     ssl_support_method  = "sni-only"
   }
 
@@ -76,13 +76,13 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
 
 ## Route 53
 resource "aws_route53_record" "web-route" {
-  zone_id = "${var.route53_zone_id}"
-  name    = "${var.url}"
+  zone_id = var.route53_zone_id
+  name    = var.url
   type    = "A"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.cloudfront_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
